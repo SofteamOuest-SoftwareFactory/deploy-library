58943aebe36c05b94402bd81792f932cb90adfd6
EL KOUHEN Mehdi <mehdi.elkouhen@gmail.com>
2018-09-27 10:21:33 +0200

    wip

--- src/com/softeam/deploy/DeployHelper.groovy
+++ src/com/softeam/deploy/DeployHelper.groovy
@@ -1,6 +1,6 @@
 package com.softeam.deploy;
 
-public class DeployHelper {
+public class DeployHelper implements Serializable {
 
     //public static final DeployHelper instance = new DeployHelper()
 
@@ -8,32 +8,32 @@ public class DeployHelper {
 
     } */
 
-    public DeployHelper() {
+    def steps
 
-        
-    }
+    DeployHelper(steps) { this.steps = steps }
+    
 
-    public void configureDockerRegistry(sh) {
-        sh 'mkdir /etc/docker'
+    public void configureDockerRegistry() {
+        steps.sh 'mkdir /etc/docker'
 
         // le registry est insecure (pas de https)
-        sh 'echo {"insecure-registries" : ["registry.k8.wildwidewest.xyz"]} > /etc/docker/daemon.json'
+        steps.sh 'echo {"insecure-registries" : ["registry.k8.wildwidewest.xyz"]} > /etc/docker/daemon.json'
 
     }
 
-    public void configureGIT(sh) {
-        sh 'mkdir /root/.ssh'
+    public void configureGIT() {
+        steps.sh 'mkdir /root/.ssh'
 
-        sh 'cp /home/jenkins/.ssh/id_rsa /root/.ssh/id_rsa'
-        sh 'cp /home/jenkins/.ssh/id_rsa.pub /root/.ssh/id_rsa.pub'
+        steps.sh 'cp /home/jenkins/.ssh/id_rsa /root/.ssh/id_rsa'
+        steps.sh 'cp /home/jenkins/.ssh/id_rsa.pub /root/.ssh/id_rsa.pub'
 
-        sh 'echo "StrictHostKeyChecking no" > /root/.ssh/config'
+        steps.sh 'echo "StrictHostKeyChecking no" > /root/.ssh/config'
 
-        sh 'chmod 600 /root/.ssh/id_rsa'
-        sh 'chmod 644 /root/.ssh/id_rsa.pub'
-        sh 'chmod 644 /root/.ssh/config'
+        steps.sh 'chmod 600 /root/.ssh/id_rsa'
+        steps.sh 'chmod 644 /root/.ssh/id_rsa.pub'
+        steps.sh 'chmod 644 /root/.ssh/config'
 
-        sh 'git config --global user.email "startech.ouest@gmail.com"'
-        sh 'git config --global user.name "Jenkins Release"'
+        steps.sh 'git config --global user.email "startech.ouest@gmail.com"'
+        steps.sh 'git config --global user.name "Jenkins Release"'
     }
 }
