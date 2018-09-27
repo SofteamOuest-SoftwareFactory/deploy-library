76162c03c9fcb3e51e2d41669b353746f2099007
EL KOUHEN Mehdi <mehdi.elkouhen@gmail.com>
2018-09-27 09:40:34 +0200

    wip

---
+++ src/com/softeam/deploy/DeployTools.groovy
@@ -0,0 +1,28 @@
+class DeployHelper {
+
+    private DeployHelper instance = new DeployHelper();
+
+    private DeployHelper() {
+
+    }
+
+    instance() {
+        return instance;
+    }
+
+    void configureGIT(sh) {
+        sh 'mkdir /root/.ssh'
+
+        sh 'cp /home/jenkins/.ssh/id_rsa /root/.ssh/id_rsa'
+        sh 'cp /home/jenkins/.ssh/id_rsa.pub /root/.ssh/id_rsa.pub'
+
+        sh 'echo "StrictHostKeyChecking no" > /root/.ssh/config'
+
+        sh 'chmod 600 /root/.ssh/id_rsa'
+        sh 'chmod 644 /root/.ssh/id_rsa.pub'
+        sh 'chmod 644 /root/.ssh/config'
+
+        sh 'git config --global user.email "startech.ouest@gmail.com"'
+        sh 'git config --global user.name "Jenkins Release"'
+    }
+}
